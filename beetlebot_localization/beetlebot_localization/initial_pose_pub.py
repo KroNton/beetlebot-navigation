f73834a4ac743f5915700ca4f38436d9b3051733
KroNton <kyrlosfekry@gmail.com>
2025-03-31 03:52:32 +0200

    Add subscription to PointStamped and update pose publishing logic

--- beetlebot_localization/beetlebot_localization/initial_pose_pub.py
+++ beetlebot_localization/beetlebot_localization/initial_pose_pub.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import PoseWithCovarianceStamped
-
+from geometry_msgs.msg import PointStamped
 
 class InitialPosePublisher(Node):
 
@@ -11,15 +11,29 @@ class InitialPosePublisher(Node):
         self.publisher_ = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.x = 0
+        self.subscription = self.create_subscription(
+            PointStamped,
+            '/clicked_point',
+            self.clicked_point_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.x = 0.0
+        self.y = 0.0
+
+    def clicked_point_callback(self, msg):
+        self.x = msg.point.x
+        self.y = msg.point.y
+        self.get_logger().info('X point: "%s"' % msg.point.x)
+        self.get_logger().info('Y point: "%s"' % msg.point.y)
 
     def timer_callback(self):
         msg = PoseWithCovarianceStamped()
-        msg.pose.pose.position.x = 0.0
-        msg.pose.pose.position.x = 0.0
+        msg.pose.pose.position.x = self.x
+        msg.pose.pose.position.y = self.y
+        msg.pose.pose.position.z = 0.0
         msg.pose.pose.orientation.z = 0.0
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.pose.pose.position.x)
+        # self.get_logger().info('Publishing: "%s"' % msg.pose.pose.position.x)
 
 
 def main(args=None):
