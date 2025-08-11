de20f95068e776aebde24e34595407ad8f7cdb5a
KroNton <kyrlosfekry@gmail.com>
2025-08-11 14:01:09 +0300

    Refactor launch file to improve readability and organization of node definitions

--- beetlebot_navigation/launch/beetlebot_nav2.launch.py
+++ beetlebot_navigation/launch/beetlebot_nav2.launch.py
@@ -1,55 +1,61 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import TimerAction
 import os
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Paths to configuration files
+
     planner_yaml = os.path.join(get_package_share_directory('beetlebot_navigation'), 'config', 'planner_server.yaml')
     controller_yaml = os.path.join(get_package_share_directory('beetlebot_navigation'), 'config', 'controller.yaml')
     bt_navigator_yaml = os.path.join(get_package_share_directory('beetlebot_navigation'), 'config', 'bt_navigator.yaml')
     recovery_yaml = os.path.join(get_package_share_directory('beetlebot_navigation'), 'config', 'recovery.yaml')
+    
+    controller_node=Node(
+        package='nav2_controller',
+        executable='controller_server',
+        name='controller_server',
+        output='screen',
+        parameters=[controller_yaml])
 
-    # Define the RViz node
-    return LaunchDescription([     
-        Node(
-            package='nav2_controller',
-            executable='controller_server',
-            name='controller_server',
-            output='screen',
-            parameters=[controller_yaml]),
+    planner_node=Node(
+        package='nav2_planner',
+        executable='planner_server',
+        name='planner_server',
+        output='screen',
+        parameters=[planner_yaml])
+        
+    recovery_node=Node(
+        package='nav2_behaviors',
+        executable='behavior_server',
+        name='recoveries_server',
+        parameters=[recovery_yaml],
+        output='screen')
 
-        Node(
-            package='nav2_planner',
-            executable='planner_server',
-            name='planner_server',
-            output='screen',
-            parameters=[planner_yaml]),
-            
-        Node(
-            package='nav2_behaviors',
-            executable='behavior_server',
-            name='recoveries_server',
-            parameters=[recovery_yaml],
-            output='screen'),
+    bt_node= Node(
+        package='nav2_bt_navigator',
+        executable='bt_navigator',
+        name='bt_navigator',
+        output='screen',
+        parameters=[bt_navigator_yaml])
 
-        Node(
-            package='nav2_bt_navigator',
-            executable='bt_navigator',
-            name='bt_navigator',
-            output='screen',
-            parameters=[bt_navigator_yaml]),
+    lifecycle_mange_node=Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_pathplanner',
+        output='screen',
+        parameters=[{'autostart': True},
+                    {'node_names': ['planner_server',
+                                    'controller_server',
+                                    'recoveries_server',
+                                    'bt_navigator']}])
+    
 
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_pathplanner',
-            output='screen',
-            parameters=[{'autostart': True},
-                        {'node_names': ['planner_server',
-                                        'controller_server',
-                                        'recoveries_server',
-                                        'bt_navigator']}])
+
+    return LaunchDescription([   
+        controller_node,
+        planner_node,
+        recovery_node,
+        bt_node,
+        lifecycle_mange_node,
     ])
